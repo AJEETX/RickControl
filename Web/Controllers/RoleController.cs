5995ba80ce88ffa70bf49071950c23c6f700bbdf
Azy <Ajeet.Tiwari@kmart.com.au>
2023-04-14 13:15:33 +0000

    add role

---
+++ Web/Controllers/RoleController.cs
@@ -0,0 +1,26 @@
+﻿
+using System.Diagnostics;
+using app.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace app.Web.Controllers
+{
+    public class RoleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
