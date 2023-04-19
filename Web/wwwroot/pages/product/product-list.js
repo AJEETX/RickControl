7cff382f2c40eda03a5df1cac1ebd09a6eae0fce
Abhinn Kumar <kumarabhinn2005@gmail.com>
2023-04-19 12:39:47 +1000

    show hide button

--- Web/wwwroot/pages/product/product-list.js
+++ Web/wwwroot/pages/product/product-list.js
@@ -67,9 +67,9 @@ $(document).ready(function () {
                     "bSortable": false,
                     "mRender": function (data, type, row) {
                         var buttons = "";
-                        buttons += '<a href="/Product/View/' + row.Id + '" class="btn btn-xs btn-warning"><i class="fa fa-search"></i> View</a>&nbsp;'
-                        buttons += '<a href="/Product/Edit/' + row.Id + '" class="btn btn-xs btn-warning"><i class="fas fa-pen"></i> Edit</a>&nbsp;'
-                        buttons += '<a onclick="deleteRow(this,' + row.Id + ')"  class="btn btn-xs btn-danger"><i class="fas fa-trash"></i> Delete</a>'
+                        buttons += '<a href="/Product/View/' + row.Id + '" style=display:' + correctRole + '; class="btn btn-xs btn-warning"><i class="fa fa-search"></i> View</a>&nbsp;'
+                        buttons += '<a href="/Product/Edit/' + row.Id + '" style=display:' + correctRole + '; class="btn btn-xs btn-warning"><i class="fas fa-pen"></i> Edit</a>&nbsp;'
+                        buttons += '<a onclick="deleteRow(this,' + row.Id + ')" style=display:' + correctRole + '; class="btn btn-xs btn-danger"><i class="fas fa-trash"></i> Delete</a>'
                         return buttons;
                     }
                 }
